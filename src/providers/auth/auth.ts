209945cff856a91b46e775a7f84f46803802c6e4
ECVD-PA-Q084-IM079 <ECVD-IM079@ECVD-PA-Q084-IM079.local>
2018-02-20 18:15:09 +0100

    start authentification

---
+++ src/providers/auth/auth.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import firebase from '@ionic-native/firebase';
+
+/*
+  Generated class for the AuthProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class AuthData {
+
+  constructor() {}
+
+}
